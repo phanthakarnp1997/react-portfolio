dceb7718fb2694508601e38b9bcf16b98c392ddc
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-09-24 00:35:18 +0700

    feat: implement sidebar, feat: change favicon

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./index.scss";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHouse,
+  faAddressCard,
+  faTimeline,
+  faLightbulb,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar() {
   return (
@@ -8,6 +17,22 @@ export default function Sidebar() {
       <Link to="/">
         <p className="text-logo">P</p>
       </Link>
+      <nav className="nav-icon">
+        <FontAwesomeIcon icon={faHouse} />
+        <FontAwesomeIcon icon={faAddressCard} />
+        <FontAwesomeIcon icon={faTimeline} />
+        <FontAwesomeIcon icon={faLightbulb} />
+        <FontAwesomeIcon icon={faEnvelope} />
+      </nav>
+
+      <ul className="nav-social">
+        <li>
+          <FontAwesomeIcon icon={faLinkedin} />
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faGithub} />
+        </li>
+      </ul>
     </div>
   );
 }
