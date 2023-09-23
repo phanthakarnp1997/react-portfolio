4d92b50648cf4617558c4fd8b8e5b2ffcabc2f84
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-09-23 22:37:46 +0700

    feat: implement sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./index.scss";
 
 export default function Sidebar() {
-  return <h1>This is Sidebar</h1>;
+  return (
+    <div className="nav-bar">
+      <Link to="/">
+        <p className="text-logo">P</p>
+      </Link>
+    </div>
+  );
 }
