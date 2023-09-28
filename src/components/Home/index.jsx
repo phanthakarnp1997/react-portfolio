13f6a7039b9c2ef222d1eaeba41ee425279602c4
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-09-28 22:02:13 +0700

    fix: change scss style

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,26 +2,18 @@ import "./index.scss";
 import React, { useState, useEffect } from "react";
 import AnimateText from "../AnimateText";
 import { TypeAnimation } from "react-type-animation";
-import Terminal from "../Terminal";
-
 export default function Home() {
   const [letterClass, setLetterClass] = useState("text-animate");
-  const sayhi = "Hi !";
+
   const name = "I'm Phanthakarn Plukpanya";
   const rolename = "Software Developer";
-  const terminalTitleBar = "Phanthakarn Plukpanya Portfolio ~";
+  const textIntroduce = `Greetings, I am Dew, an experienced software developer with a passion
+  for problem-solving. I am eager to expand my knowledge of cutting-edge
+  technologies and embrace fresh challenges to further enhance my
+  skillset.`;
 
-  const descriptionTypingComponent = (
-    <TypeAnimation
-      speed={50}
-      sequence={[
-        `Greetings, I am Dew, an experienced software developer with a passion
-          for problem-solving. I am eager to expand my knowledge of cutting-edge
-          technologies and embrace fresh challenges to further enhance my
-          skillset.`,
-        1000,
-      ]}
-    />
+  const typingIntroduceComponent = (
+    <TypeAnimation speed={50} sequence={[textIntroduce, 1000]} />
   );
 
   useEffect(() => {
@@ -31,23 +23,16 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="home-page">
-      <div className="information">
-        <AnimateText
-          idx={5}
-          letterClass={`say-hi ${letterClass}`}
-          text={sayhi}
-        />
-        <AnimateText idx={10} letterClass={letterClass} text={name} />
-        <div className="role">
-          <AnimateText idx={35} letterClass={letterClass} text={rolename} />
+    <div className="container home-page">
+      <div className="wrapper">
+        <div className="text-zone information">
+          <AnimateText idx={5} letterClass={`${letterClass}`} text="Hi!" />
+          <AnimateText idx={10} letterClass={letterClass} text={name} />
+          <div>
+            <AnimateText idx={35} letterClass={letterClass} text={rolename} />
+          </div>
+          <div className={`description`}>{typingIntroduceComponent}</div>
         </div>
-      </div>
-      <div className={`description`}>
-        <Terminal
-          titleBar={terminalTitleBar}
-          itemDisplay={descriptionTypingComponent}
-        />
       </div>
     </div>
   );
