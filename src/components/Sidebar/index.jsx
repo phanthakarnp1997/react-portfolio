0142fedef737c439697b1cacf11f087fae8864da
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-09-30 15:59:49 +0700

    feat: make responsive side-bar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -13,9 +13,15 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <div className="nav-bar">
+      <div className={`nav-bar ${isOpen ? "open" : ""}`}>
         <div className="menu">
           <Link to="/">
             <p className="text-logo">P</p>
@@ -25,9 +31,10 @@ export default function Sidebar() {
             color="#ffd700"
             size="3x"
             className="hamburger-icon text-logo"
+            onClick={toggleSidebar}
           />
         </div>
-        <nav className="nav-icon">
+        <nav className={`nav-icon ${isOpen ? "open" : ""}`}>
           <div className="icon-hover">
             <FontAwesomeIcon icon={faHouse} />
             <span>Home</span>
@@ -49,7 +56,7 @@ export default function Sidebar() {
             <span>Contact</span>
           </div>
         </nav>
-        <ul className="nav-social">
+        <ul className={`nav-social ${isOpen ? "open" : ""}`}>
           <li>
             <a
               href="https://www.linkedin.com/in/phanthakarn-plukpanya-358863193/"
