3a2ed37356833f6106d8a10725945edb20baca23
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-10-01 17:58:46 +0700

    feat: toggled navbar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./index.scss";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,63 +15,76 @@ import {
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  let rootRef = useRef();
+
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    let handler = (e) => {
+      if (isOpen && !rootRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [isOpen]);
+
   return (
-    <>
-      <div className={`nav-bar ${isOpen ? "open" : ""}`}>
-        <div className="menu">
-          <Link to="/">
-            <p className="text-logo">P</p>
-          </Link>
-          <FontAwesomeIcon
-            icon={faBars}
-            color="#ffd700"
-            size="3x"
-            className="hamburger-icon text-logo"
-            onClick={toggleSidebar}
-          />
-        </div>
-        <nav className={`nav-icon ${isOpen ? "open" : ""}`}>
-          <div className="icon-hover">
-            <FontAwesomeIcon icon={faHouse} />
-            <span>Home</span>
-          </div>
-          <div className="icon-hover">
-            <FontAwesomeIcon icon={faAddressCard} />
-            <span>About Me</span>
-          </div>
-          <div className="icon-hover">
-            <FontAwesomeIcon icon={faTimeline} />
-            <span>Experience</span>
-          </div>
-          <div className="icon-hover">
-            <FontAwesomeIcon icon={faLightbulb} />
-            <span>Skill</span>
-          </div>
-          <div className="icon-hover">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <span>Contact</span>
-          </div>
-        </nav>
-        <ul className={`nav-social ${isOpen ? "open" : ""}`}>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/phanthakarn-plukpanya-358863193/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedin} className="icon-hover" />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/phanthakarnp1997" target="_blank">
-              <FontAwesomeIcon icon={faGithub} className="icon-hover" />
-            </a>
-          </li>
-        </ul>
+    <div className={`nav-bar ${isOpen ? "open" : ""}`} ref={rootRef}>
+      <div className="menu">
+        <Link to="/">
+          <p className="text-logo">P</p>
+        </Link>
+        <FontAwesomeIcon
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className="hamburger-icon text-logo"
+          onClick={toggleSidebar}
+        />
       </div>
-    </>
+      <nav className={`nav-icon ${isOpen ? "open" : ""}`}>
+        <div className="icon-hover">
+          <FontAwesomeIcon icon={faHouse} />
+          <span>Home</span>
+        </div>
+        <div className="icon-hover">
+          <FontAwesomeIcon icon={faAddressCard} />
+          <span>About Me</span>
+        </div>
+        <div className="icon-hover">
+          <FontAwesomeIcon icon={faTimeline} />
+          <span>Experience</span>
+        </div>
+        <div className="icon-hover">
+          <FontAwesomeIcon icon={faLightbulb} />
+          <span>Skill</span>
+        </div>
+        <div className="icon-hover">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span>Contact</span>
+        </div>
+      </nav>
+      <ul className={`nav-social ${isOpen ? "open" : ""}`}>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/phanthakarn-plukpanya-358863193/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="icon-hover" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/phanthakarnp1997" target="_blank">
+            <FontAwesomeIcon icon={faGithub} className="icon-hover" />
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
