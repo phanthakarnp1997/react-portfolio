2b34c679a3e1aef5c833297d6f516ba2beadfd95
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-10-01 18:46:07 +0700

    fix: layout path

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/react-portfolio" element={<Layout />}>
           <Route index element={<Home />}></Route>
         </Route>
       </Routes>
