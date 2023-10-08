15c26c95d4478185f2c8eb26fee60300501ac972
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-10-08 15:12:22 +0700

    feat: timeline-component

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -2,19 +2,87 @@ import withLoader from "../../utils/withLoader";
 import "./index.scss";
 import React from "react";
 import AnimateText from "../AnimateText";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBriefcase,
+  faGraduationCap,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Experience() {
   return (
     <>
       <div className="container experience-page">
         <h1>
-          {" "}
           <AnimateText
             letterClass="text-animate-hover"
             text="Experience"
             idx={1}
           />
         </h1>
+        <div className="text-zone">
+          <VerticalTimeline>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date="2019 - present"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<FontAwesomeIcon icon={faBriefcase} />}
+            >
+              <h3 className="vertical-timeline-element-title">Programmer</h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                CDG Systems Co. Ltd
+              </h4>
+              <p>Responisibilty...</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date="Jul 2018 - Oct 2018"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<FontAwesomeIcon icon={faBriefcase} />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                Software Developer (Internship)
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                G-Able Public Company Limited
+              </h4>
+              <p>Responisibilty...</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date="Sep 2016 - Mar 2019"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<FontAwesomeIcon icon={faGraduationCap} />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                Student (Computer Science)
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                Rajamangala University of Technology Krungthep
+              </h4>
+              <p>
+                Farmer Robot Project Using Arduino, MQTT and
+                (HTML,CSS,JavaScript)
+              </p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
       </div>
     </>
   );
