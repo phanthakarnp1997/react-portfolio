790b24efdbaec2e48ee99e7cb00f88b3756642a9
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-10-11 18:42:49 +0700

    feat: init skill-page

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -1,12 +1,15 @@
 import withLoader from "../../utils/withLoader";
 import "./index.scss";
 import React from "react";
+import AnimateText from "../AnimateText";
 
 function Skill() {
   return (
     <>
-      <div className="page-header">
-        <div className="text-zone">Skill</div>
+      <div className="container skill-page">
+        <h1>
+          <AnimateText letterClass="text-animate-hover" text="Skill" idx={1} />
+        </h1>
       </div>
     </>
   );
