56d9df2d89e4e04af21f1b2272dec1115854b4e2
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-10-13 15:11:07 +0700

    feat: contact-form layout

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,12 +1,29 @@
 import "./index.scss";
 import React from "react";
 import withLoader from "../../utils/withLoader";
+import AnimateText from "../AnimateText";
 
 function Contact() {
   return (
     <>
-      <div className="page-header">
-        <div className="text-zone">Contact</div>
+      <div className="container contact-page">
+        <h1>
+          <AnimateText
+            idx={1}
+            letterClass="text-animate-hover"
+            text="Contact Me"
+          />
+        </h1>
+        <div className="form-container">
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Subject" />
+            <textarea type="text" placeholder="Message" />
+
+            <input type="submit" value="Send"></input>
+          </form>
+        </div>
       </div>
     </>
   );
