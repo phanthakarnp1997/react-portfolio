ff7d36c500f1b68e3e9be0b39d67f7f6a76bc9c6
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-10-15 00:40:48 +0700

    fix: github-pages 404 on refresh #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import About from "./components/About";
