0f11621a89568c09c88ed3d53fe7514cd20d409c
dew_phanthakarn <phanthakarn.plukpanya@gmail.com>
2023-10-14 22:04:24 +0700

    feat: form-validator

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,8 +2,27 @@ import "./index.scss";
 import React from "react";
 import withLoader from "../../utils/withLoader";
 import AnimateText from "../AnimateText";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { object, string } from "yup";
 
 function Contact() {
+  let schema = object({
+    name: string().required(),
+    email: string().email().required(),
+    subject: string().required(),
+    message: string().required(),
+  });
+  const { register, handleSubmit, formState } = useForm({
+    defaultValues: {},
+    resolver: yupResolver(schema),
+  });
+
+  const { errors } = formState;
+
+  const handleSave = (formValues) => {
+    console.log(formValues);
+  };
   return (
     <>
       <div className="container contact-page">
@@ -15,13 +34,45 @@ function Contact() {
           />
         </h1>
         <div className="form-container">
-          <form>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Subject" />
-            <textarea type="text" placeholder="Message" />
-
-            <input type="submit" value="Send"></input>
+          <form onSubmit={handleSubmit(handleSave)}>
+            <div>
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                autoComplete={"new-password"}
+                placeholder="Name"
+              />
+              <div className="validate-message">{errors.name?.message}</div>
+            </div>
+            <div>
+              <input
+                {...register("email")}
+                type="text"
+                autoComplete={"new-password"}
+                placeholder="Email"
+              />
+              <div className="validate-message">{errors.email?.message}</div>
+            </div>
+            <div>
+              <input
+                {...register("subject")}
+                type="text"
+                autoComplete={"new-password"}
+                placeholder="Subject"
+              />
+              <div className="validate-message">{errors.subject?.message}</div>
+            </div>
+            <div>
+              <textarea
+                {...register("message")}
+                type="text"
+                placeholder="Message"
+              />
+              <div className="validate-message">{errors.message?.message}</div>
+            </div>
+            <div>
+              <input type="submit" value="Send"></input>
+            </div>
           </form>
         </div>
       </div>
